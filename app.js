b0eec527ac05e9362ccf2f18447e2c4a7c5e5677
Francisco Portilha <portilhafr@gmail.com>
2023-03-05 05:11:44 +0100

    menu with fadeIn animation

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
 const menuButton = document.getElementById("menu-button");
 const menuList = document.querySelector("nav ul");
-const menuItems = document.querySelectorAll("ul li");
 
 menuButton.addEventListener("click", () => {
-    
-    setTimeout(() => {
-        menuList.classList.toggle("show");
-        
-    },200)
-
-    setTimeout(() => {
-        for (let i = 0 ; i < menuItems.length();i++){
-            menuItems[i].classList.toggle("show");
-        }
-    },1000)
+    setTimeout(() => {menuList.classList.toggle("show");},150)
 });
-
-
 
