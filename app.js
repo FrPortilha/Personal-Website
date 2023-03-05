2d82bc2fbddaf1e1c840ee44d0bb027c171cbed6
Francisco Portilha <portilhafr@gmail.com>
2023-03-05 15:44:21 +0100

    menubug fixed

--- app.js
+++ app.js
@@ -5,13 +5,3 @@ const menuList = document.querySelector("nav ul");
 menuButton.addEventListener("click", () => {
     setTimeout(() => {menuList.classList.toggle("show");},150)
 });
-
-
-import ScrollMagic from "scrollmagic";
-
-new ScrollMagic.Scene({
-    triggerElement: "#trigger1"
-})
-.setClassToggle("#reveal1", "visible") // add class to reveal
-.addIndicators() // add indicators (requires plugin)
-.addTo(controller);
