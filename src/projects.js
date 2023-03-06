ee3bde8451dcad83baaeba5ccb9aaec6ce3343ff
Francisco Portilha <portilhafr@gmail.com>
2023-03-06 20:24:21 +0100

    project menu with iterative pictures

---
+++ src/projects.js
@@ -0,0 +1,16 @@
+
+
+const prj1 = document.getElementById("p1");
+const box1 = document.getElementById("b1");
+prj1.addEventListener("mouseover", ()=>{ box1.classList.toggle("show"); });
+prj1.addEventListener("mouseleave", ()=>{ box1.classList.toggle("show"); });
+
+const prj2 = document.getElementById("p2");
+const box2 = document.getElementById("b2");
+prj2.addEventListener("mouseover", ()=>{ box2.classList.toggle("show"); });
+prj2.addEventListener("mouseleave", ()=>{ box2.classList.toggle("show"); });
+
+const prj3 = document.getElementById("p3");
+const box3 = document.getElementById("b3");
+prj3.addEventListener("mouseover", ()=>{ box3.classList.toggle("show"); });
+prj3.addEventListener("mouseleave", ()=>{ box3.classList.toggle("show"); });
